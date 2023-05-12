73ed268f6a16d5679a913d7d5ecb09b28e93f9e9
albjoh <aljo940616@gmail.com>
2023-05-12 18:38:21 +0200

    paint brain and make it grow

--- src/components/Brain.tsx
+++ src/components/Brain.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface BrainProps {
   setCount: (count: number) => void;
@@ -6,15 +6,19 @@ interface BrainProps {
 }
 
 const Brain: FC<BrainProps> = ({ setCount, count }) => {
+  const [scale, setScale] = useState(1);
   const handleClick = () => {
     setCount(count + 1);
+    setScale(scale + 0.00001);
   };
   return (
-    <div
-      onClick={handleClick}
-      style={{ backgroundColor: "brown", padding: "20px", cursor: "pointer" }}
-    >
-      <p style={{ userSelect: "none" }}>Brain</p>
+    <div onClick={handleClick} style={{ padding: "20px", cursor: "pointer" }}>
+      <img
+        id="brain"
+        src="/brain.svg"
+        width={"200px"}
+        style={{ userSelect: "none", scale: `${scale}` }}
+      />
     </div>
   );
 };
